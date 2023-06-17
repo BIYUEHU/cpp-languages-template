a4ffab614f7a6afee541f4dd09947952f113ab4c
BIYUEHU <biyuehuya@gmail.com>
2023-06-17 16:49:33 +0800

    add some three js files

--- usr/theme/default/user/footer.php
+++ usr/theme/default/user/footer.php
@@ -8,8 +8,10 @@
 </main>
 <script src="//cdn.staticfile.org/layui/2.8.7/layui.js"></script>
 <script src="//cdn.staticfile.org/jquery/3.7.0/jquery.min.js"></script>
+<script src="/index.php/assets/js/user/popper.min.js"></script>
 <script src="//cdn.staticfile.org/popper.js/2.11.8/umd/popper.min.js"></script>
-<script src="//cdn.staticfile.org/bootstrap/5.3.0/js/bootstrap.min.js"></script>
+<!-- <script src="//cdn.staticfile.org/bootstrap/5.3.0/js/bootstrap.min.js"></script> -->
+<script src="/index.php/assets/js/user/bootstrap.min.js"></script>
 <script src="/index.php/assets/js/user/main.js"></script>
 <script src="/index.php/assets/js/user/index.js"></script>
 <script src="/index.php/assets/js/index.js"></script>
