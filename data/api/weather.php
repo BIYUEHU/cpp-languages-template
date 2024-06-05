33941aee780922173cb8f7108210b7d824ea9b19
Hotaru <biyuehuya@gmail.com>
2024-06-05 18:08:31 +0800

    feat: weather api return josn

--- data/api/weather.php
+++ data/api/weather.php
@@ -1,58 +1,78 @@
 <?php
-header('Content-Type: text/plain');
-
-function jiequ($txt1, $q1, $h1)
-{
+function handle($txt1, $q1, $h1) {
     $txt1 = strstr($txt1, $q1);
     $cd = strlen($q1);
     $txt1 = substr($txt1, $cd);
     $txt1 = strstr($txt1, $h1, true);
-    return $txt1;
+    return str_replace(' ','',$txt1);
 }
+
+header('Content-Type: application/json');
+
 $msg = $_REQUEST['msg'];
-$b = $_REQUEST['b'];
-$list = file_get_contents("http://m.moji.com/api/citysearch/" . $msg);
-$result = preg_match_all("/{\"cityId\":(.*?),\"city_lable\":(.*?),\"counname\":\"(.*?)\",\"id\":(.*?),\"localCounname\":\"(.*?)\",\"localName\":\"(.*?)\",\"localPname\":\"(.*?)\",\"name\":\"(.*?)\",\"pname\":\"(.*?)\"}/", $list, $nute);
-if ($b == null) {
-    for ($x = 0; $x < $result && $x <= 9; $x++) {
-        $jec = $nute[6][$x];
-        $je = $nute[7][$x];
-        echo ($x + 1) . "：" . $je . "-" . $jec . "\n";
-    }
-    echo "提示：发送以上序号选择，例：选墨迹天气 1";
-} else
-if ($b > 10 || $b < 1) {
-    echo "请按以上序号选择";
+
+// 检查参数是否为空
+if (empty($msg)) {
+    echo json_encode([
+        'code' => 501,
+        'message' => 'fail: parameter cannot be empty'
+    ]);
+    exit;
+}
+
+// 获取城市列表数据
+preg_match_all("/{\"cityId\":(.*?),\"city_lable\":(.*?),\"counname\":\"(.*?)\",\"id\":(.*?),\"localCounname\":\"(.*?)\",\"localName\":\"(.*?)\",\"localPname\":\"(.*?)\",\"name\":\"(.*?)\",\"pname\":\"(.*?)\"}/", file_get_contents("http://m.moji.com/api/citysearch/" . $msg), $matches);
+
+if (!$matches[1][0]) {
+    echo json_encode([
+        'code' => 502,
+        'message' => 'fail: parameter error'
+    ]);
+    exit;
+}
+
+// 正则表达式提取天气详情
+$weatherInfo = handle(file_get_contents("http://m.moji.com/api/redirect/" . $matches[1][0]), "<div class=\"weak_wea\">", "<div class=\"exponent\">");
+preg_match_all("/<lidata-high=\"(.*?)\"data-low=\"(.*?)\">/", $weatherInfo, $temperatureMatches);
+preg_match_all("/<em>(.*?)<\/em>/", $weatherInfo, $dateMatches);
+preg_match_all("/<dd><strong>(.*?)<\/strong><\/dd>/", $weatherInfo, $weatherConditionMatches);
+preg_match_all("/<pclass=\"(.*?)\">(.*?)<\/p><dlclass=\"wind\">/", $weatherInfo, $airQualityMatches);
+preg_match_all("/<dd>(.*?)<\/dd>/", $weatherInfo, $windMatches);
+
+if ($temperatureMatches) {
+    echo json_encode([
+        'code' => 500,
+        'message' => 'success',
+        'data' => [
+            'city' => $matches[6][0],
+            'list' => [
+                [
+                    'date' => $dateMatches[1][0], // 日期
+                    'temperature' => [intval($temperatureMatches[2][0]), intval($temperatureMatches[1][0])], 
+                    'condition' => $weatherConditionMatches[1][0], // 天气情况
+                    'wind' => [$windMatches[1][2], $windMatches[1][3]], // 风度
+                    'air_quality' => $airQualityMatches[2][0] // 空气质量
+                ],
+                [
+                    'date' => $dateMatches[1][1],
+                    'temperature' => [intval($temperatureMatches[2][1]), intval($temperatureMatches[1][1])],
+                    'condition' => $weatherConditionMatches[1][1],
+                    'wind' => [$windMatches[1][6], $windMatches[1][7]],
+                    'air_quality' => $airQualityMatches[2][1]
+                ],
+                [
+                    'date' => $dateMatches[1][2],
+                    'temperature' => [intval($temperatureMatches[2][2]), intval($temperatureMatches[1][2])],
+                    'condition' => $weatherConditionMatches[1][2],
+                    'wind' => [$windMatches[1][10], $windMatches[1][11]],
+                    'air_quality' => $airQualityMatches[2][2]
+                ]
+            ]
+        ]
+    ]);
 } else {
-    $b = $_REQUEST['b'];
-    $b = ($b - 1);
-    $je = $nute[1][$b];
-    $jec = $nute[6][$b];
-    $lis = file_get_contents("http://m.moji.com/api/redirect/" . $je);
-    $bb = jiequ($lis, "<div class=\"weak_wea\">", "<div class=\"exponent\">");
-    $bb = str_replace(' ', '', $bb);
-    preg_match_all("/<lidata-high=\"(.*?)\"data-low=\"(.*?)\">/", $bb, $aa);
-    preg_match_all("/<em>(.*?)<\/em>/", $bb, $qq);
-    preg_match_all("/<dd><strong>(.*?)<\/strong><\/dd>/", $bb, $cc);
-    preg_match_all("/<pclass=\"(.*?)\">(.*?)<\/p><dlclass=\"wind\">/", $bb, $dd);
-    preg_match_all("/<dd>(.*?)<\/dd>/", $bb, $ee);
-    preg_match_all("/<dd>(.*?)<\/dd>/", $bb, $ff);
-    echo "城市：" . $jec . "\n";
-    echo "日期：" . $qq[1][0] . "\n";
-    echo "温度：" . $aa[2][0] . "～" . $aa[1][0] . "℃\n";
-    echo "天气：" . $cc[1][0] . "\n";
-    echo "风度：" . $ee[1][2] . "-" . $ff[1][3] . "\n";
-    echo "空气质量：" . $dd[2][0] . "\n";
-    echo "\n";
-    echo "日期：" . $qq[1][1] . "\n";
-    echo "温度：" . $aa[2][1] . "～" . $aa[1][1] . "℃\n";
-    echo "天气：" . $cc[1][1] . "\n";
-    echo "风度：" . $ee[1][6] . "-" . $ff[1][7] . "\n";
-    echo "空气质量：" . $dd[2][1] . "\n";
-    echo "\n";
-    echo "日期：" . $qq[1][2] . "\n";
-    echo "温度：" . $aa[2][2] . "～" . $aa[1][2] . "℃\n";
-    echo "天气：" . $cc[1][2] . "\n";
-    echo "风度：" . $ee[1][10] . "-" . $ff[1][11] . "\n";
-    echo "空气质量：" . $dd[2][2] . "";
+    echo json_encode([
+        'code' => 502,
+        'message' => 'fail: parameter error'
+    ]);
 }
